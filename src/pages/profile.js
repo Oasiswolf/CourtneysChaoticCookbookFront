c7371e82b457b3fca5ef233e6061206df9a6a02a
qmkimball <quinnkimball192@gmail.com>
2022-02-04 14:22:25 -0700

    Profile Commit

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 
 export default class Profile extends Component {
-  constructor(props) {
+  constructor() {
     super();
 
     this.state = {};
   }
 
   render() {
-    return <div className="">saved recipes</div>;
+    return (
+      <div className="portfolio-wrapper">
+        <h1>Your Portfolio</h1>
+        {/* username from JS cookies */}
+        <h2>Here you'll find your saved recipes, comment statuses</h2>
+
+        <div className="recipe-wrapper">
+          <h3>SAVED RECIPES</h3>
+          {/* recipes render herer */}
+        </div>
+        <div className="comment-wrapper">
+          <h3>COMMENTS</h3>
+          {/* comments on recipes */}
+        </div>
+      </div>
+    );
   }
 }
